90be035f1b63be8cb664f16912e7e9f5153a795f
CarrionAndOn <maya@carrionandon.me>
2023-03-07 17:34:56 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+# Create a log file for everything the script is doing
+LOG_FILE=$(dirname $0)/install.log
+touch $LOG_FILE
+
 # Check if psutil is installed
 if ! command -v pip3 &> /dev/null
 then
     # Install psutil
-    sudo apt-get update
-    sudo apt-get install python3-pip -y
-    pip3 install psutil
+	echo Installing psutil...
+    sudo apt-get update >> $LOG_FILE 2>&1 >/dev/null
+    sudo apt-get install python3-pip -y >> $LOG_FILE 2>&1 >/dev/null
+    pip3 install psutil >> $LOG_FILE 2>&1 >/dev/null
 fi
 
 # Download the usage monitor python script to /home
-wget https://raw.githubusercontent.com/CarrionAndOn/Linux-Usage-Monitor/main/usagemonitor.py -P /home
+echo Installing script...
+wget https://raw.githubusercontent.com/CarrionAndOn/Linux-Usage-Monitor/main/usagemonitor.py -P /home >> $LOG_FILE 2>&1 >/dev/null
 
 # Prompt the user for the Discord webhook URL
 read -p "Enter the Discord webhook URL: " WEBHOOK_URL
 
 # Replace the webhook URL in the usagemonitor.py script with the user's input
-sed -i "s|https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN|$WEBHOOK_URL|g" /home/usagemonitor.py
+sed -i "s|https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN|$WEBHOOK_URL|g" /home/usagemonitor.py >> $LOG_FILE 2>&1 >/dev/null
 
 # Create a systemd service for the script
-cat <<EOF | sudo tee /etc/systemd/system/usagemonitor.service
+echo Creating service...
+cat <<EOF | sudo tee /etc/systemd/system/usagemonitor.service >> $LOG_FILE 2>&1 >/dev/null
 [Unit]
 Description=Usage Monitor Service
 
@@ -33,6 +40,9 @@ WantedBy=multi-user.target
 EOF
 
 # Start and enable the service
-sudo systemctl daemon-reload
-sudo systemctl start usagemonitor
-sudo systemctl enable usagemonitor
+echo Enabling service...
+sudo systemctl daemon-reload >> $LOG_FILE 2>&1 >/dev/null
+sudo systemctl start usagemonitor >> $LOG_FILE 2>&1 >/dev/null
+sudo systemctl enable usagemonitor >> $LOG_FILE 2>&1 >/dev/null
+echo Installed! Check your discord channel where the webhook was set up.
+set +x
