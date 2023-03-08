1775f3a866bbed5d75897e1c26dfa0c42e49f05f
CarrionAndOn <maya@carrionandon.me>
2023-03-07 20:17:00 -0800

    Changed the script itself back to use vcgencmd

--- usagemonitor.py
+++ usagemonitor.py
@@ -6,8 +6,11 @@ import time
 import psutil
 import os
 
-# Get CPU temperature
-cpu_temp = psutil.sensors_temperatures()['coretemp'][0].current
+# Get the temperature and capture the output
+output = subprocess.check_output(['vcgencmd', 'measure_temp']).decode('utf-8')
+
+# Extract the temperature value from the output using a regular expression
+temperature = re.findall(r'\d+\.\d+', output)[0]
 
 # Get CPU usage
 cpu_usage = psutil.cpu_percent()
@@ -18,7 +21,7 @@ total_ram = round(ram.total / (1024.0 ** 3), 2)
 used_ram = round(ram.used / (1024.0 ** 3), 2)
 
 # Build the message to send to the Discord webhook
-message = f"Temp: {cpu_temp}°C\nRAM: {used_ram} GB used of {total_ram} GB\nCPU: {cpu_usage}%"
+message = f"Temp: {temperature}°C\nRAM: {used_ram} GB used of {total_ram} GB\nCPU: {cpu_usage}%"
 
 # Define the Discord webhook URL
 webhook_url = "https://discord.com/api/webhooks/WEBHOOK_ID/WEBHOOK_TOKEN"
