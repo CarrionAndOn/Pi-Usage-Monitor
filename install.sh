090347d106fed63057c7e0bba87a7557b6e3bd7e
CarrionAndOn <maya@carrionandon.me>
2023-03-07 22:08:56 -0800

    Fixed config generation

--- install.sh
+++ install.sh
@@ -25,11 +25,11 @@ touch /opt/usagemonitordiscord/usagemonitor.conf
 # Prompt the user for the Discord webhook URL
 read -p "Enter the Discord webhook URL: " WEBHOOK_URL
 # Add that to the config
-echo "$sWebhook: $WEBHOOK_URL" >> /opt/usagemonitordiscord/usagemonitor.conf
+echo "Webhook: $WEBHOOK_URL" >> /opt/usagemonitordiscord/usagemonitor.conf
 # Prompt the user for the desired time between each message
 read -p "Enter the time between each message in seconds: " TIME
 # Add that to the config too
-echo "$s\nTime(Seconds): $TIME" >> /opt/usagemonitordiscord/usagemonitor.conf
+echo "Time(Seconds): $TIME" >> /opt/usagemonitordiscord/usagemonitor.conf
 
 # Move script to /opt to run
 sudo mv ~/usagemonitor.py /opt/usagemonitordiscord/
