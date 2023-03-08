639be1fbcbc81f544709001e7f1afb39c2d376f7
CarrionAndOn <maya@carrionandon.me>
2023-03-07 22:52:00 -0800

    Checks for if running on Pi
    I want this to be compatible outside of the Pi.
    It'll check for if it's running on Pi before running commands that are exclusive to the Pi.

--- usagemonitor.py
+++ usagemonitor.py
@@ -5,6 +5,7 @@ import json
 import time
 import psutil
 import os
+import platform
 
 # Set the config file path and get it's values
 with open('/opt/usagemonitor.conf', 'r') as file:
@@ -12,11 +13,13 @@ with open('/opt/usagemonitor.conf', 'r') as file:
 webhook_url = config_data.split("Webhook: ")[1].split("\n")[0]
 time_seconds = int(config_data.split('Time(Seconds): ')[1].split('\n')[0])
 
+# Check if system is a Pi
+if platform.system() == "Linux" and platform.machine() in ["armv7l", "armv8"]:
 # Get the temperature and capture the output
 output = subprocess.check_output(['vcgencmd', 'measure_temp']).decode('utf-8')
-
 # Extract the temperature value from the output using a regular expression
 temperature = re.findall(r'\d+\.\d+', output)[0]
+else
 
 # Get CPU usage
 cpu_usage = psutil.cpu_percent()
@@ -26,8 +29,13 @@ ram = psutil.virtual_memory()
 total_ram = round(ram.total / (1024.0 ** 3), 2)
 used_ram = round(ram.used / (1024.0 ** 3), 2)
 
+# Check for Pi again
+if platform.system() == "Linux" and platform.machine() in ["armv7l", "armv8"]:
 # Build the message to send to the Discord webhook
 message = f"Temp: {temperature}°C\nRAM: {used_ram} GB used of {total_ram} GB\nCPU: {cpu_usage}%"
+else:
+# Build the message to send to the Discord webhook
+message = f"RAM: {used_ram} GB used of {total_ram} GB\nCPU: {cpu_usage}%"
 
 # Build the payload to send to the webhook
 payload = {
